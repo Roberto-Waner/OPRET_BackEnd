5a8053b41a508a4bbcf42618c82f6f68af5dacb8
Roberto Waner <waner301@gmail.com>
2024-10-25 16:36:53 -0400

    pequeña correciones por parte de adaptacion de uno de los stored procedure y que mas adelate la base de datos, el backEnd y el fronTEnd sufriran grandes cambios debido al almacenamiento en la nubes con Azure y la memoria cache con sqlLite

--- WebApiForm/WebApiForm/Services/DTO (Data Transfer Object)/PreguntaCompleta.cs
+++ WebApiForm/WebApiForm/Services/DTO (Data Transfer Object)/PreguntaCompleta.cs
@@ -2,9 +2,9 @@
 {
     public class PreguntaCompleta
     {
-        public int CodPregunta { get; set; }
+        public int? CodPregunta { get; set; }
         public string? TipoRespuesta { get; set; }
-        public string Pregunta { get; set; }
+        public string? Pregunta { get; set; }
         public string? SubPregunta { get; set; }
         public string? Rango { get; set; }
     }
